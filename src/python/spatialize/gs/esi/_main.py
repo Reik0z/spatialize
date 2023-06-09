a2783ff8f369acfe4a46cb4d857855f86e328d15
fcowied <alvarorama@gmail.com>
2023-06-08 20:21:30 -0400

    finishing the grid search first version

--- src/python/spatialize/gs/esi/_main.py
+++ src/python/spatialize/gs/esi/_main.py
@@ -20,7 +20,7 @@ else:
                     common_args={"k": 10,
                                  "griddata": False,
                                  "n_partitions": [100],
-                                 "alpha": list(np.flip(np.arange(0.97, 0.981, 0.01))),
+                                 "alpha": list(np.flip(np.arange(0.70, 0.90, 0.01))),
                                  "agg_function": {"mean": af.mean, "median": af.median},
                                  "show_progress": True},
                     specific_args={
@@ -34,6 +34,7 @@ def esi_hparams_search(points, values, xi, **kwargs):
     if k == points.shape[0] or k == -1:
         method = "loo"
 
+    # get the cross validation function
     cross_validate = LibSpatializeFacade.get_operator(points, kwargs["base_interpolator"], method)
 
     grid = {"n_partitions": kwargs["n_partitions"],
@@ -47,12 +48,12 @@ def esi_hparams_search(points, values, xi, **kwargs):
         grid["nugget"] = kwargs["nugget"]
         grid["range"] = kwargs["range"]
 
+    # get the actual parameter grid
+    param_grid = ParameterGrid(grid)
+
     p_xi = xi
     if kwargs["griddata"]:
         p_xi, _ = flatten_grid_data(xi)
-
-    # get the actual parameter grid
-    param_grid = ParameterGrid(grid)
 
     # run the scenarios
     results = {}
@@ -60,11 +61,10 @@ def esi_hparams_search(points, values, xi, **kwargs):
     if kwargs["show_progress"]:
         it = tqdm(range(len(param_grid)), desc="searching the grid")
     for i in it:
-        param_set = param_grid[i]
-        args_dict = param_set.copy()
-        args_dict["base_interpolator"] = kwargs["base_interpolator"]
+        param_set = param_grid[i].copy()
+        param_set["base_interpolator"] = kwargs["base_interpolator"]
 
-        l_args = build_arg_list(points, values, p_xi, args_dict)
+        l_args = build_arg_list(points, values, p_xi, param_set)
         if method == "kfold":
             l_args.insert(-1, k)
 
@@ -75,8 +75,19 @@ def esi_hparams_search(points, values, xi, **kwargs):
 
     # sort results
     results = dict(sorted(results.items(), key=lambda x: x[1], reverse=False))
-    for k, v in results.items():
-        print(f"({k[0], param_grid[k[1]]}) ---> {v}")
+
+    # look for the best params combination
+    best_key = list(results.keys())[0]
+    best_params = param_grid[best_key[1]]
+    best_params["agg_function"] = kwargs["agg_function"][best_key[0]]
+    best_params["base_interpolator"] = kwargs["base_interpolator"]
+
+    # this is just a reminder that a better way to return
+    # search results needs to be implemented
+    #
+    # for k, v in results.items():
+    #     print(f"({k[0], param_grid[k[1]]}) ---> {v}")
+    return best_params
 
 
 def esi_griddata(points, values, xi, **kwargs):
@@ -100,7 +111,6 @@ def esi_nongriddata(points, values, xi, **kwargs):
                         "kriging": {"model": 1, "nugget": 0.1, "range": 5000.0}
                     })
 def _call_libspatialize(points, values, xi, **kwargs):
-
     # get the estimator function
     estimate = LibSpatializeFacade.get_operator(points, kwargs["base_interpolator"], "estimate")
 
@@ -186,14 +196,13 @@ def flatten_grid_data(xi):
 
 
 def build_arg_list(points, values, xi, nonpos_args):
-
     # add common args
     l_args = [np.float32(points), np.float32(values), nonpos_args["n_partitions"], nonpos_args["alpha"], np.float32(xi)]
 
     # add specific args
     if nonpos_args["base_interpolator"] == "idw":
         l_args.insert(-1, nonpos_args["exponent"])
-        
+
     if nonpos_args["base_interpolator"] == "kriging":
         l_args.insert(-1, LibSpatializeFacade.get_kriging_model_number(nonpos_args["model"]))
         l_args.insert(-1, nonpos_args["nugget"])
--- test/esi_test.py
+++ test/esi_test.py
@@ -80,14 +80,11 @@ class ESIModule(unittest.TestCase):
             self.assertEqual(str(e), "Base interpolator 'rbf' not supported")
 
     def test_griddata(self):
-        def func(x, y):  # a kind of cubic function
-            return x * (1 - x) * np.cos(4 * np.pi * x) * np.sin(4 * np.pi * y ** 2) ** 2
-
         grid_x, grid_y = np.mgrid[0:1:100j, 0:1:200j]
 
         rng = np.random.default_rng()
         points = rng.random((1000, 2))
-        values = func(points[:, 0], points[:, 1])
+        values = self.func(points[:, 0], points[:, 1])
 
         import spatialize.gs.esi.aggfunction as af
         import spatialize.gs.esi.precfunction as pf
@@ -106,17 +103,21 @@ class ESIModule(unittest.TestCase):
                             agg_function=af.mean, prec_function=pf.mae_precision)
 
     def test_hparams_search(self):
-        def func(x, y):  # a kind of cubic function
-            return x * (1 - x) * np.cos(4 * np.pi * x) * np.sin(4 * np.pi * y ** 2) ** 2
-
         grid_x, grid_y = np.mgrid[0:1:100j, 0:1:200j]
 
         rng = np.random.default_rng()
         points = rng.random((1000, 2))
-        values = func(points[:, 0], points[:, 1])
+        values = self.func(points[:, 0], points[:, 1])
 
         from spatialize.gs.esi import esi_hparams_search
-        esi_hparams_search(points, values, (grid_x, grid_y), base_interpolator="kriging", griddata=True, k=10)
+        b_params = esi_hparams_search(points, values, (grid_x, grid_y),
+                                      base_interpolator="kriging", griddata=True, k=-1,
+                                      alpha=list(np.flip(np.arange(0.70, 0.75, 0.01))))
+        print(b_params)
+
+    @staticmethod
+    def func(x, y):  # a kind of "cubic" function
+        return x * (1 - x) * np.cos(4 * np.pi * x) * np.sin(4 * np.pi * y ** 2) ** 2
 
 
 if __name__ == '__main__':
