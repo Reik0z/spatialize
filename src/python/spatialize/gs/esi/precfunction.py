61f57a4b3f61cbe0e03ad4fc3db41b6422e5dde1
fcowied <alvarorama@gmail.com>
2023-06-10 22:46:41 -0400

    Still working on notebooks

--- src/python/spatialize/gs/esi/precfunction.py
+++ src/python/spatialize/gs/esi/precfunction.py
@@ -15,8 +15,22 @@ def mae_precision(estimation, esi_samples):
                                mean)
 
 
-def apply_loss_function(estimation, esi_samples, loss_function, agg_function):
+class OpErrorPrecision:
+    def __init__(self, dyn_range=None):
+        self.dyn_range = dyn_range
 
+    def __call__(self, estimation, esi_samples):
+        dyn_range = self.dyn_range
+        if dyn_range is None:
+            dyn_range = np.abs(np.min(estimation) - np.max(estimation))
+
+        def op_error(x, y):
+            return np.abs(x - y) / dyn_range
+
+        return apply_loss_function(estimation, esi_samples, op_error, mean)
+
+
+def apply_loss_function(estimation, esi_samples, loss_function, agg_function):
     loss = np.empty(esi_samples.shape)
     for i in range(loss.shape[1]):
         loss[:, i] = loss_function(esi_samples[:, i], estimation)
