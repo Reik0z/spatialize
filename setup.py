0756576ee2981b49bca19edee13cfda1acf2a276
Felipe Navarro V <fnavarrov@gmail.com>
2023-07-07 14:40:49 -0400

    Related files for packaging

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ macros = [('NPY_NO_DEPRECATED_API', 'NPY_1_7_API_VERSION')]
 libspatialize_extensions = [
     Extension(name='libspatialize',
               sources=[libsrc],
-              include_dirs=[numpy.get_include(), os.path.join('.', 'include')],
+              include_dirs=[ os.path.join('.', 'include'), numpy.get_include()],
               extra_compile_args=['-std=c++14'],
               define_macros=macros,
               ),
@@ -18,7 +18,7 @@ libspatialize_extensions = [
 
 if __name__ == '__main__':
     setup(
-        name='specialize',
+        name='spatialize',
         version='0.1',
         author='ALGES Laboratory',
         author_email='dev@alges.cl',
@@ -28,9 +28,8 @@ if __name__ == '__main__':
         long_description=open(os.path.join(os.path.dirname(os.path.realpath(
             __file__)), "README.txt")).read(),
         ext_modules=libspatialize_extensions,
-        # packages=['spatialize'],
-        # package_dir={'justesi': 'src'},
-        # packages=find_packages('..', exclude=[".DS_Store", "__pycache__"]),
+        package_dir={'spatialize': os.path.join('src', 'python', 'spatialize')},
+        packages=find_packages(os.path.join('src', 'python'), exclude=[".DS_Store", "__pycache__"]),
         include_package_data=True,
         scripts=[],
         install_requires=[
