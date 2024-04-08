9256ec80cce50c476ac74f05b3a1ca235cb78f23
fcowied <alvarorama@gmail.com>
2024-04-07 22:20:53 -0400

    Trying some other stuff to improve performance

--- include/spatialite/abstract_esi.hpp
+++ include/spatialite/abstract_esi.hpp
@@ -110,9 +110,9 @@ namespace sptlz{
 
 				sqlite3_exec(this->db, "PRAGMA synchronous = OFF", NULL, NULL, &err_msg);
                 sqlite3_exec(this->db, "PRAGMA journal_mode = MEMORY", NULL, NULL, &err_msg);
-                sqlite3_exec(this->db, "PRAGMA temp_store = 2", NULL, NULL, &err_msg);
-                sqlite3_exec(this->db, "PRAGMA cache_size=-300000", NULL, NULL, &err_msg);
-
+                sqlite3_exec(this->db, "PRAGMA temp_store = MEMORY", NULL, NULL, &err_msg);
+                sqlite3_exec(this->db, "PRAGMA cache_size=-1000000", NULL, NULL, &err_msg);
+                sqlite3_exec(this->db, "PRAGMA locking_mode=EXCLUSIVE", NULL, NULL, &err_msg);
 
 				// ask if database was open ok
 				if(rc) {
