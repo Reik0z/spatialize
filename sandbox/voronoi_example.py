28d5989aa5d7d8f9caf0ad611f03595955b1225f
fcowied <alvarorama@gmail.com>
2024-06-04 17:46:44 -0400

    Working on sandbox

---
+++ sandbox/voronoi_example.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import time
+from voronoi.ensemble import EnsembleIDW
+
+trees = 50
+alpha = 0.7
+
+nugg_case = 'nugg0.1'
+file_sim = f'sim_data_{nugg_case}.csv'
+data_sim = pd.read_csv(file_sim)
+grid = data_sim[['X', 'Y']]
+
+col_sim = 'sim1'
+case_list = ['5perc']
+
+
+s = time.time()
+file_samples = f'sim_data_{nugg_case}_{case_list[0]}.csv'
+data_samples = pd.read_csv(file_samples)
+samples = data_samples[['X', 'Y', col_sim]]
+
+esi = EnsembleIDW(trees, alpha, samples, grid, value_col=col_sim)
+result = esi.predict()
+
+grid_result = grid.copy()
+est_col = 'esi_original_' + col_sim
+grid_result[est_col] = result.estimates
+
+out_filename = f'esi_voronoi_{nugg_case}_{str(alpha)}_{str(trees)}_{case_list[0]}.csv'
+grid_result.to_csv(out_filename, index=False)
+print(f"elapsed time: {time.time() - s:.2f}")
