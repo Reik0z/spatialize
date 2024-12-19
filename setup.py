3e3bd34818cefba84a5df5b8dbecab57297dacce
Felipe Navarro V. <fnavarrov@gmail.com>
2024-12-19 11:58:07 -0300

    Removing SQLite dependencies from main

--- setup.py
+++ setup.py
@@ -7,7 +7,6 @@ import pathlib
 import pkg_resources
 
 libsptlzsrc = os.path.join('src', 'c++', 'libspatialize.cpp')
-libsptltsrc = os.path.join('src', 'c++', 'libspatialite.cpp')
 macros = [('NPY_NO_DEPRECATED_API', 'NPY_1_7_API_VERSION')]
 
 with pathlib.Path('requirements.txt').open() as requirements_txt:
@@ -20,13 +19,6 @@ with pathlib.Path('requirements.txt').open() as requirements_txt:
 libspatialize_extensions = [
     Extension(name='libspatialize',
               sources=[libsptlzsrc],
-              include_dirs=[ os.path.join('.', 'include'), numpy.get_include()],
-              extra_compile_args=['-std=c++14'],
-              define_macros=macros,
-              ),
-    Extension(name='libspatialite',
-              sources=[libsptltsrc],
-              libraries = ['sqlite3'],
               include_dirs=[ os.path.join('.', 'include'), numpy.get_include()],
               extra_compile_args=['-std=c++14'],
               define_macros=macros,
