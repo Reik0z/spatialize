2ab75772559ddfb0d69544ace25c922bf393fb45
aehrenfeld <aehrenfeld@alges.cl>
2025-01-05 19:20:54 -0300

    3d scripted example renamed and plotting improvement

--- examples/scripted_examples/esi_3d_nongriddata.py
+++ examples/scripted_examples/esi_3d_nongriddata.py
@@ -1,10 +1,9 @@
-import numpy as np
 from matplotlib import pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.gridspec as gridspec
 
 from spatialize import logging
 import spatialize.gs.esi.aggfunction as af
-from spatialize.gs.esi import esi_griddata
+from spatialize.gs.esi import esi_nongriddata
 from spatialize.data import load_drill_holes_andes_3D
 
 
@@ -14,39 +13,40 @@ locations = locations.sort_values(["z", "y", "x"])
 
 logging.log.setLevel("DEBUG")
 
+#input variables for estimation
 points = samples[['x', 'y', 'z']].values
 x, y, z = samples[['x']].values, samples[['y']].values, samples[['z']].values
 values = samples[['cu']].values[:, 0]
 
+#3d locations for estimatimation values
+xi = locations[['x', 'y', 'z']].values
+grid_x, grid_y, grid_z = locations[['x']].values, locations[['y']].values, locations[['z']].values
 
-fig = plt.figure()
-ax = fig.add_subplot(projection='3d')
+#estimation function
+result = esi_nongriddata(points, values, xi,
+                      local_interpolator="idw",
+                      p_process="mondrian",
+                      exponent=1.0,
+                      n_partitions=100,
+                      alpha=0.7,
+                      agg_function=af.mean
+                      )
 
-ax.scatter3D(
+#plotting original data and resulting estimation points
+fig = plt.figure(dpi=150, figsize=(10,5))
+G = gridspec.GridSpec(1, 2)
+
+ax1 = fig.add_subplot(G[0,0], projection='3d')
+ax1.set_title('3d samples')
+ax1.scatter3D(
         x, y, z,
         c=values,
         cmap='coolwarm'
 )
 
-plt.show()
-
-grid_x, grid_y, grid_z = locations[['x']].values, locations[['y']].values, locations[['z']].values
-
-result = esi_griddata(points, values, (grid_x, grid_y, grid_z),
-                      local_interpolator="idw",
-                      p_process="mondrian",
-                      data_cond=False,
-                      exponent=1.0,
-                      n_partitions=100, alpha=0.7,
-                      agg_function=af.mean
-                      )
-
-print(result.estimation())
-
-fig = plt.figure()
-ax = fig.add_subplot(projection='3d')
-
-ax.scatter3D(
+ax2 = fig.add_subplot(G[0,1], projection='3d')
+ax2.set_title('esi idw 3d estimation')
+ax2.scatter3D(
         grid_x, grid_y, grid_z,
         c=result.estimation(),
         cmap='coolwarm'
