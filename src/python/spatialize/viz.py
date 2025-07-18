34c2fa51bf409732cc6faae65cc3305220994d98
Jesu <mvalenzuela@alges.cl>
2025-07-18 16:50:32 -0400

    Updated color palettes

--- src/python/spatialize/viz.py
+++ src/python/spatialize/viz.py
@@ -3,9 +3,9 @@ import random as rd
 from spatialize import in_notebook, logging
 
 import matplotlib.pyplot as plt
-from matplotlib.pyplot import colorbar
+#from matplotlib.pyplot import colorbar
 from mpl_toolkits.axes_grid1 import make_axes_locatable
-import matplotlib.colors as mcolors
+from matplotlib.colors import LinearSegmentedColormap as Colormap
 
 from spatialize import SpatializeError
 from spatialize.logging import log_message
@@ -13,8 +13,18 @@ from spatialize.logging import log_message
 PALETTES = {
     'alges': ['#070f15', '#0f212d', '#19384d', '#275474', '#326c94', '#4b87af', '#78abce', '#a7c9e1', '#cfe2ef'],
     'alges_muted': ['#142b3b','#1e4058', '#285676', '#326c94', '#5a89a9', '#84a6be', '#c1d2de'],
-    'noname': ['#ffcbb4', '#ad92be', '#786aa9', '#124dc5'],
-    'crest': ['#7dba91', '#59a590', '#40908e', '#287a8c', '#1c6488', '#254b7f']}
+    'navia_r': ['#011a5a','#104261','#205f61','#4c734d','#828434','#c09036','#f2a069','#fcb3b3','#fee5e5'],
+    'precision': ['#51648a','#796982','#9d6c79','#c47673','#e98c76','#edb18e','#edd3b3'],
+    'precision_dark': ['#0b1425','#133859','#48587a','#6c5e74','#90606c','#bc6461','#e67960','#eb937f','#f2bcaf'],
+    'precision_muted': ['#5b6f9a','#867590','#a77c87','#cb8785','#ed9e8c','#f0c0a4','#f2dfc7'],
+    'crest': ['#a4ceb2', '#7dba91', '#59a590', '#40908e', '#287a8c', '#1c6488', '#254b7f', '#193458'],
+    'batlow': ['#fbcdfa','#fcb3b3','#f2a069','#c09036','#828434','#4c734d','#205f61','#104261','#011a5a'],
+    'glasgow': ['#371437','#4e1921','#6a2810','#74471c','#716328','#697c47','#61917c','#74a8af','#a8bed7','#dcd1e8'],
+    'lipari': ['#0b1425','#133859','#48587a','#6c5e74','#90606c','#bc6461','#e67960','#e9a278','#e8c79e','#fef5db'],
+    'navia': ['#0b1627','#16345c','#19598c','#29738e','#398285','#4b9379','#66aa6a','#98ca6e','#d9e5a6','#fcf5d9'],
+    'nuuk': ['#11598c','#2c6384','#4b7182','#70868c','#939b96','#acad95','#bbb98b','#c7c581','#e1e08c','#fbf7b3'],
+    'bamako': ['#073a46','#12433f','#214f33','#385d2c','#547032','#738437','#978e33','#bfa830','#e2c66b','#fee4ab'],
+    'tokio': ['#1f1032','#4b1f42','#6a404e','#715651','#746651','#767a54','#7d9857','#8ec26d','#c3e0a7','#eff5db']}
 
 class PlotStyle:
     """
@@ -61,6 +71,7 @@ class PlotStyle:
                 'axes.facecolor': '#EAEAF2',
                 'axes.edgecolor': 'white',
                 'axes.labelweight': 'demibold',
+                'axes.titleweight': 'bold',
                 'figure.titleweight': 'bold',
                 'grid.color': 'white',
                 'xtick.bottom': False,
@@ -69,8 +80,8 @@ class PlotStyle:
                 'ytick.labelsize': 'small'
             },
             'color': '#59a590',
-            'cmap': mcolors.LinearSegmentedColormap.from_list(
-                'crest', PALETTES['crest'], N=256),
+            'cmap': Colormap.from_list('crest', PALETTES['crest']),
+            'precision_cmap': Colormap.from_list('custom_precision', PALETTES['precision'])
         },
         'whitegrid': {
             'rcparams': {
@@ -79,6 +90,7 @@ class PlotStyle:
                 'axes.facecolor': 'white',
                 'axes.edgecolor': '#EAEAF2',
                 'axes.labelweight': 'demibold',
+                'axes.titleweight': 'bold',
                 'figure.titleweight': 'bold',
                 'grid.color': '#EAEAF2',
                 'xtick.bottom': False,
@@ -87,8 +99,8 @@ class PlotStyle:
                 'ytick.labelsize': 'small'
             },
             'color': '#7dba91',
-            'cmap': mcolors.LinearSegmentedColormap.from_list(
-                'crest', PALETTES['crest'], N=256),
+            'cmap': Colormap.from_list('crest', PALETTES['crest']),
+            'precision_cmap': Colormap.from_list('custom_precision', PALETTES['precision'])
         },
         'dark': {
             'rcparams': {
@@ -98,6 +110,7 @@ class PlotStyle:
                 'axes.edgecolor': '#1e2832',
                 'axes.labelcolor': 'white',
                 'axes.labelweight': 'demibold',
+                'axes.titleweight': 'bold',
                 'figure.titleweight': 'bold',
                 'text.color': 'white',
                 'xtick.color': '#b3b9c1',
@@ -107,7 +120,8 @@ class PlotStyle:
                 'ytick.labelsize': 'small', 
             },
             'color': '#24a187',
-            'cmap': 'viridis'
+            'cmap':  Colormap.from_list('custom_precision', PALETTES['navia_r']),
+            'precision_cmap': Colormap.from_list('custom_precision', PALETTES['precision_dark'])
         },
         'white': {
             'rcparams': {
@@ -115,6 +129,7 @@ class PlotStyle:
                 'axes.grid': False,
                 'axes.facecolor': 'white',
                 'axes.edgecolor': '#EAEAF2',
+                'axes.titleweight': 'bold',
                 'grid.color': '#EAEAF2',
                 'xtick.labelsize': 'small',
                 'ytick.labelsize': 'small',
@@ -124,8 +139,8 @@ class PlotStyle:
                 'ytick.left': True,
             },
             'color': '#7dba91',
-            'cmap': mcolors.LinearSegmentedColormap.from_list(
-                'crest', PALETTES['crest'], N=256),
+            'cmap': Colormap.from_list('crest', PALETTES['crest']),
+            'precision_cmap': Colormap.from_list('custom_precision', PALETTES['precision'])
         },
         'alges': {
             'rcparams': {
@@ -135,6 +150,7 @@ class PlotStyle:
                 'axes.edgecolor': '#808080',
                 'axes.labelcolor': '#424e77',
                 'axes.labelweight': 'demibold',
+                'axes.titleweight': 'bold',
                 'figure.titleweight': 'bold',
                 'xtick.labelsize': 'small',
                 'ytick.labelsize': 'small',
@@ -146,8 +162,8 @@ class PlotStyle:
                 'ytick.left': True
             },
             'color': "#8fb4cd",
-            'cmap': mcolors.LinearSegmentedColormap.from_list(
-                'alges_cmap', PALETTES['alges'], N=256)
+            'cmap': Colormap.from_list('alges_cmap', PALETTES['alges']),
+            'precision_cmap': Colormap.from_list('custom_precision', PALETTES['precision'])
         },
         'alges_muted': {
             'rcparams': {
@@ -157,6 +173,7 @@ class PlotStyle:
                 'axes.edgecolor': '#808080',
                 'axes.labelcolor': '#545f84',
                 'axes.labelweight': 'demibold',
+                'axes.titleweight': 'bold',
                 'figure.titleweight': 'bold',
                 'xtick.labelsize': 'small',
                 'ytick.labelsize': 'small',
@@ -168,8 +185,8 @@ class PlotStyle:
                 'ytick.left': True
             },
             'color': "#84a6be",
-            'cmap': mcolors.LinearSegmentedColormap.from_list(
-                'alges_cmap', PALETTES['alges_muted'], N=256)
+            'cmap': Colormap.from_list('alges_muted', PALETTES['alges_muted']),
+            'precision_cmap': Colormap.from_list('custom_precision', PALETTES['precision_muted'])
         },
         'minimal': {
             'rcparams': {
@@ -178,7 +195,8 @@ class PlotStyle:
                 'axes.spines.right': False,
             },
             'color': '#333333',
-            'cmap': 'copper'
+            'cmap': 'binary_r',
+            'precision_cmap': 'binary_r',
         },
         'publication': {
             'rcparams': {
@@ -187,11 +205,13 @@ class PlotStyle:
                 'grid.color': '#E0E0E0',
                 'axes.spines.top': False,
                 'axes.spines.right': False,
+                'axes.titleweight': 'bold',
                 'font.family': 'serif',
                 'font.size': 10,
             },
             'color': '#000000',
-            'cmap': 'cividis'
+            'cmap': Colormap.from_list('nuuk', PALETTES['nuuk']),
+            'precision_cmap': Colormap.from_list('custom_precision', PALETTES['precision_muted'])
         }
     }
 
@@ -219,26 +239,26 @@ class PlotStyle:
             self._apply_theme()
 
     def _set_color(self, color):
-        if color is not None:
+        if color:
             return color
-        elif self.theme is not None:
+        elif self.theme:
             return self.THEMES[self.theme]['color']
         else:
             return self.DEFAULT_COLOR
     
     def _set_cmap(self, cmap):
-        if cmap is not None:
+        if cmap:
             return cmap
-        elif self.theme is not None:
+        elif self.theme:
             return self.THEMES[self.theme]['cmap']
         else:
             return self.DEFAULT_CMAP
         
     def _set_precision_cmap(self, cmap):
-        if cmap is not None:
+        if cmap:
             return cmap
-        #elif self.theme is not None:
-        #    return self.THEMES[self.theme]['cmap']
+        elif self.theme:
+            return self.THEMES[self.theme]['precision_cmap']
         else:
             return self.DEFAULT_PRECISION_CMAP
 
@@ -274,7 +294,7 @@ class PlotStyle:
         self.reset_to_original()
 
 
-def plot_histogram(data, ax, style, alpha = 0.9, rwidth=0.92, show_empty = False):
+def plot_histogram(data, ax, color, alpha = 0.9, rwidth=0.92, show_empty = False):
     """
     Plots a histogram of the given data using matplotlib
     :param data: The data to visualize. Should be either a 1D array-like object 
@@ -286,7 +306,7 @@ def plot_histogram(data, ax, style, alpha = 0.9, rwidth=0.92, show_empty = False
     :param show_empty: Whether to show tick labels for empty histogram bins. Default assigns False.
     """
     counts, bin_edges, _ = ax.hist(data,
-                                   color=style.color,
+                                   color=color,
                                    alpha=alpha,
                                    rwidth=rwidth,
                                    zorder=3)
@@ -301,7 +321,7 @@ def plot_histogram(data, ax, style, alpha = 0.9, rwidth=0.92, show_empty = False
     ax.xaxis.set_major_formatter(plt.FuncFormatter(lambda x, p: f'{x:.3f}'))
 
 
-def plot_colormap_data(data, ax=None, w=None, h=None, xi_locations=None, griddata=False, title="", **figargs):
+def plot_colormap_data(data, ax=None, w=None, h=None, xi_locations=None, griddata=False, title="", **imshow_args):
     """
     Plots a colormap (heatmap-like visualization) of the given data using matplotlib.
 
@@ -313,7 +333,7 @@ def plot_colormap_data(data, ax=None, w=None, h=None, xi_locations=None, griddat
     :param xi_locations: Coordinates of the data points, used to infer shape (h, w) if not provided explicitly.
     :param griddata: If True, assumes `data` is already in 2D format and transposes it for display.
     :param title: Title to display on the plot (only used if `ax` is None and a new figure is created).
-    :param figargs: Additional keyword arguments to pass to `imshow()`, such as `cmap`, `vmin`, or `vmax`.
+    :param imshow_args: Additional keyword arguments to pass to `imshow()`, such as `cmap`, `vmin`, or `vmax`.
 
     :raises SpatializeError: If neither `w`/`h` nor `xi_locations` are provided when `griddata` is False,
                              or if reshaping fails due to inconsistent dimensions.
@@ -339,14 +359,15 @@ def plot_colormap_data(data, ax=None, w=None, h=None, xi_locations=None, griddat
         plotter = gs.subplots()
         plotter.set_title(title)
 
-    img = plotter.imshow(im, origin='lower', **figargs)
+    img = plotter.imshow(im, origin='lower', **imshow_args)
     divider = make_axes_locatable(plotter)
     cax = divider.append_axes("right", size="5%", pad=0.1)
-    colorbar(img, orientation='vertical', cax=cax)
+    cax.grid(False)
+    plt.colorbar(img, orientation='vertical', cax=cax)
 
 
 def plot_colormap_array(data, n_imgs=9, n_cols=3, norm_lims=False, xi_locations=None, reference_map=None,
-                        cmap='coolwarm', title="", title_prefix="scenario", seed=None, **figargs):
+                        cmap='coolwarm', title="", title_prefix="scenario", seed=None, **imshow_args):
     """
       Plots an array of colormap visualizations (heatmaps) for a subset of data columns in a grid layout.
 
@@ -361,7 +382,7 @@ def plot_colormap_array(data, n_imgs=9, n_cols=3, norm_lims=False, xi_locations=
       :param title: The title for the entire plot. If `None`, no title will be set.
       :param title_prefix: Prefix for individual subplot titles (e.g., "scenario 1", "scenario 2").
       :param seed: The seed for random number generation. If `None`, a random seed will be generated for reproducibility.
-      :param figargs: Additional arguments to pass to the `plot_colormap_data` function, such as `vmin` or `vmax`.
+      :param imshow_args: Additional arguments to pass to the `plot_colormap_data` function, such as `vmin` or `vmax`.
 
       :return: A Matplotlib figure object and the seed used for random sampling.
       :rtype: matplotlib.figure.Figure, int
@@ -404,7 +425,7 @@ def plot_colormap_array(data, n_imgs=9, n_cols=3, norm_lims=False, xi_locations=
                            xi_locations=xi_locations,
                            cmap=cmap,
                            vmin=vmin, vmax=vmax,
-                           **figargs)
+                           **imshow_args)
         axis[i].set_title(f"{title_prefix} {sim_idx[i] + 1}")
 
     for i in range(n_imgs, n_rows * n_cols):
